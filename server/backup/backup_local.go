36dd4fbfc0c0b8fd6378fad133c47a1197924b2d
Kamesuta <kamesuta@gmail.com>
2025-02-12 09:47:15 +0900

    [KPW] サーバーごとに違うフォルダで保存

--- server/backup/backup_local.go
+++ server/backup/backup_local.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"io"
 	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"emperror.dev/errors"
 	"github.com/juju/ratelimit"
@@ -16,25 +20,34 @@ import (
 
 type LocalBackup struct {
 	Backup
+
+	// Name of backup file.
+	Name string `json:"name"`
+
+	// UUID of server
+	Server string `json:"server"`
 }
 
 var _ BackupInterface = (*LocalBackup)(nil)
 
-func NewLocal(client remote.Client, uuid string, ignore string) *LocalBackup {
+func NewLocal(client remote.Client, uuid string, ignore string, name string, server string) *LocalBackup {
 	return &LocalBackup{
-		Backup{
+		Backup: Backup{
 			client:  client,
 			Uuid:    uuid,
 			Ignore:  ignore,
 			adapter: LocalBackupAdapter,
+			path:    LocalPath(uuid, name, server),
 		},
+		Name: name,
+		Server: server,
 	}
 }
 
 // LocateLocal finds the backup for a server and returns the local path. This
 // will obviously only work if the backup was created as a local backup.
-func LocateLocal(client remote.Client, uuid string) (*LocalBackup, os.FileInfo, error) {
-	b := NewLocal(client, uuid, "")
+func LocateLocal(client remote.Client, uuid string, server string) (*LocalBackup, os.FileInfo, error) {
+	b := NewLocal(client, uuid, "", "", server)
 	st, err := os.Stat(b.Path())
 	if err != nil {
 		return nil, nil, err
@@ -45,6 +58,65 @@ func LocateLocal(client remote.Client, uuid string) (*LocalBackup, os.FileInfo, 
 	}
 
 	return b, st, nil
+}
+
+// SanitizeFilename はファイル名に使用できない文字を削除し、安全なファイル名に変換する
+func SanitizeFilename(name string) string {
+	// Windows と Unix で使用できない文字を除去する
+	invalidChars := regexp.MustCompile(`[<>:"/\\|?*\x00-\x1F]`)
+	name = invalidChars.ReplaceAllString(name, "")
+
+	// 先頭と末尾のピリオドと空白を削除
+	name = strings.Trim(name, " .")
+
+	// 半角スペースを "_" に置換
+	name = strings.ReplaceAll(name, " ", "_")
+
+	return name
+}
+
+// LocalPath returns the path on the system for a given backup UUID and server name.
+func LocalPath(uuid string, name string, server string) string {
+	backupDir := config.Get().System.BackupDirectory
+
+	// デフォルト形式の「UUID.tar.gz」があればそれを使用
+	{
+		filePath := path.Join(backupDir, uuid+".tar.gz")
+		if _, err := os.Stat(filePath); err == nil {
+			return filePath
+		}
+	}
+
+	// 「サーバー名～～」フォルダがあればそれを使用、ない場合「サーバー名」をフォルダ名として使用する
+	serverDir := path.Join(backupDir, server+"*")
+	{
+		matches, err := filepath.Glob(serverDir)
+		if err == nil && len(matches) > 0 {
+			serverDir = matches[0]
+		} else {
+			serverDir = path.Join(backupDir, server)
+
+			// フォルダがない場合は作成
+			if _, err := os.Stat(serverDir); os.IsNotExist(err) {
+				if err := os.Mkdir(serverDir, 0755); err != nil {
+					return ""
+				}
+			}
+		}
+	}
+
+	// 「UUID～～.tar.gz」ファイルがあればそれを使用、ない場合「UUID_.tar.gz」をファイル名として使用する
+	filePath := path.Join(serverDir, uuid+"*.tar.gz")
+	{
+		matches, err := filepath.Glob(filePath)
+		if err == nil && len(matches) > 0 {
+			filePath = matches[0]
+		} else {
+			filePath = path.Join(serverDir, uuid+"_"+SanitizeFilename(name)+".tar.gz")
+		}
+	}
+
+	return filePath
 }
 
 // Remove removes a backup from the system.
